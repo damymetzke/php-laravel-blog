10bfd2e1f8c40eb3a990e5f750b8091b1df8095e
Damy Metzke <damy.metzke@gmail.com>
2020-11-02 09:50:04 +0100

    Implement posts for admin panel.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,6 +4,21 @@
     <link rel="stylesheet" href="/res/css/admin.css">
 @endsection
 
+@section('navigation')
+    <li><a href="/admin/create">Create Post</a></li>
+@endsection
+
 @section('content')
     <h1>Admin</h1>
+    <ol id="posts">
+        @foreach ($posts as $post)
+            <li>
+                <a href="admin/post/{{$post->id}}/edit">
+                    <h3>{{$post->title}}</h3>
+                    <p>{{substr($post->body, 0, 100)}}...</p>
+                    <p>Click to read more</p>
+                </a>
+            </li>
+        @endforeach
+    </ol>
 @endsection
