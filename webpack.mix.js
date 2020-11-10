351d51a94fddcefb1b69a06dcf1815eab2e575e0
Damy Metzke <damy.metzke@gmail.com>
2020-11-10 09:30:39 +0100

    Undo the automatic rewrite of 'webpack.mix.js'.

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,32 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+// .postCss('resources/css/app.css', 'public/css', [
+//     //
+// ]);
+mix
+    .js('resources/js/edit-post.ts', 'public/res/js')
+    .js('resources/js/create-post.ts', 'public/res/js')
+
+    .sass('resources/sass/root.scss', 'public/res/css')
+    .sass('resources/sass/posts.scss', 'public/res/css')
+    .sass('resources/sass/admin.scss', 'public/res/css')
+    .sass('resources/sass/post.scss', 'public/res/css')
+    .sass('resources/sass/edit-post.scss', 'public/res/css')
+    .sass('resources/sass/create-post.scss', 'public/res/css')
+    .webpackConfig({
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: "ts-loader",
+                    exclude: /node_modules/
+                }
+            ]
+        }
+    });
