47bb8963029819426274b2d8cda228353cef9787
Damy Metzke <damy.metzke@gmail.com>
2020-11-11 11:07:43 +0100

    Add input errors to login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,10 +10,12 @@
     <div>
         <label for="email">Email</label>
         <input type="email" id="email" name="email" autofocus>
+        <x-input-error type="email"></x-input-error>
     </div>
     <div>
         <label for="password">Password</label>
         <input type="password" id="password" name="password">
+        <x-input-error type="password"></x-input-error>
     </div>
 
     <button type="submit">Login</button>
