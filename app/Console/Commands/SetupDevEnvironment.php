9630101c0a9e24cac26db47cdb503004e9ec6c21
Damy Metzke <damy.metzke@gmail.com>
2020-11-30 17:44:56 +0100

    Add second step to setup command. Step: generate key.

--- app/Console/Commands/SetupDevEnvironment.php
+++ app/Console/Commands/SetupDevEnvironment.php
@@ -42,40 +42,55 @@ class SetupDevEnvironment extends Command
      */
     public function handle()
     {
-        $globalProgress = $this->output->createProgressBar(1);
+        $globalProgress = $this->output->createProgressBar(2);
 
-        $this->info('Step 1/1: setup .env file');
+        $globalProgress->display();
+        $this->info(' STEP: setup .env file');
 
         if (file_exists('.env')) {
             $this->info('.env file already exists, skipping step.');
-            exit;
-        }
+            $this->envContent = file_get_contents('.env');
+        } else {
 
-        $this->envContent = file_get_contents('.env.example');
-        $variables = [
-            'DB_HOST' =>        $this->setupEnvironmentVariable('host',         'DB_HOST',      '127.0.0.1'),
-            'DB_PORT' =>        $this->setupEnvironmentVariable('port',         'DB_PORT',      '3306'),
-            'DB_DATABASE' =>    $this->setupEnvironmentVariable('database',     'DB_DATABASE',  'laravel'),
-            'DB_USERNAME' =>    $this->setupEnvironmentVariable('username',     'DB_USERNAME',  'root'),
-            'DB_PASSWORD' =>    $this->setupEnvironmentVariable('password',     'DB_PASSWORD',  '')
-        ];
+            $this->envContent = file_get_contents('.env.example');
+            $variables = [
+                'DB_HOST' =>        $this->setupEnvironmentVariable('host',         'DB_HOST',      '127.0.0.1'),
+                'DB_PORT' =>        $this->setupEnvironmentVariable('port',         'DB_PORT',      '3306'),
+                'DB_DATABASE' =>    $this->setupEnvironmentVariable('database',     'DB_DATABASE',  'laravel'),
+                'DB_USERNAME' =>    $this->setupEnvironmentVariable('username',     'DB_USERNAME',  'root'),
+                'DB_PASSWORD' =>    $this->setupEnvironmentVariable('password',     'DB_PASSWORD',  '')
+            ];
 
-        $printing =
-            "Setting Enviroment Variables:\n";
+            $printing =
+                "Setting Enviroment Variables:\n";
 
-        foreach ($variables as $environmentName => $value) {
-            if ($value === '') {
-                continue;
+            foreach ($variables as $environmentName => $value) {
+                if ($value === '') {
+                    continue;
+                }
+
+                $printing .= "\t" . $environmentName . '=' . $value . "\n";
             }
 
-            $printing .= "\t" . $environmentName . '=' . $value . "\n";
+            $this->info($printing);
+
+            file_put_contents('.env', $this->envContent);
+        }
+        $globalProgress->advance();
+
+        $globalProgress->display();
+        $this->info(' STEP: generate application key');
+
+        if (preg_match('/APP_KEY=[a-zA-Z0-9\:\=]/', $this->envContent) === 1) {
+            $this->info('App key found, skipping step');
+        } else {
+            $this->call('key:generate');
         }
 
-        $this->info($printing);
-
-        file_put_contents('.env', $this->envContent);
 
         $globalProgress->advance();
+        $this->newLine();
+
 
         return 0;
     }
