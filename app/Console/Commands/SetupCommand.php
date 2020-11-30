dfcac96c7f4ffa09a20a557381ba23d49e524c9e
Damy Metzke <12676439+damymetzke@users.noreply.github.com>
2020-11-30 22:13:42 +0100

    Update setup command. (#33)
    * Update help text for setup command.
    
    * remove up.sh.
    replaced by artisan command 'app:setup'.
    
    * Rename command to SetupCommand.

--- app/Console/Commands/SetupCommand.php
+++ app/Console/Commands/SetupCommand.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class SetupDevEnvironment extends Command
+class SetupCommand extends Command
 {
     /**
      * The name and signature of the console command.
@@ -21,7 +21,9 @@ class SetupDevEnvironment extends Command
     protected $description = 'Quickly setup a valid development environment';
 
     protected $help = "Does the following steps:\n"
-        . "\t1. Generate environment file";
+        . "\t1. Generate environment file\n"
+        . "\t2. Generate application key\n"
+        . "\t3. Migrate and seed database\n";
 
     private $envContent = '';
 
